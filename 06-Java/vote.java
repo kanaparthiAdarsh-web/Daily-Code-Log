ddf3ffc1b46126815f94b4e42da51c63d61681e7
Adarsh Kanaparthi <kanaparthiadarsh@gmail.com>
2026-07-23 23:23:25 +0530

    Seeking the candidate was used for both of our mouths.
    This program checks whether a candidate is eligible to vote. It may be challenging at the start, but I use it in previous and future programs.

---
+++ 06-Java/vote.java
@@ -0,0 +1,16 @@
+class Scratch {
+    public static void main(String[] args) {
+        int a = 18;
+        boolean id = true;
+        if (a >= 18) {
+
+            if (id == true) {
+                System.out.println("eligible");
+            } else {
+                System.out.println("not eligible or missing id");
+            }
+        } else {
+            System.out.println("Not eligible Under age");
+        }
+    }
+}
